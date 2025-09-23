e1e7ef57136ea738dfc43caef4f2f7f49f656e6a
Mathew-Steeve <mthwsteeve@gmail.com>
2025-09-23 17:45:19 +0530

    task2 completed

--- task2/greet.php
+++ task2/greet.php
@@ -5,10 +5,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $message = htmlspecialchars($_POST['message']);
 
     // Display a message using the collected data
-    echo "<p>Welcome" . $name . "</p>";
+    echo "<p>Welcome " . $name . "</p>";
     echo "<p>". $message . "</p>";
 } else {
     // If the script is accessed directly without a POST request
     echo "<p>Please submit the form.</p>";
 }
 ?>
+<!-- command to start server : php -S localhost:port -->
+ <!-- run (http://localhost:8000/file/path) in browser not in live server -->
